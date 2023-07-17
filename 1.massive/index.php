9c3cf367e0df5eea699ad7fb57ec5900d3bb0e32
yngpass <139801560+yngpass@users.noreply.github.com>
2023-07-17 20:49:17 +0300

    Add files via upload

---
+++ 1.massive/index.php
@@ -0,0 +1,36 @@
+<?php
+    include 'data.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Массивы</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+    <table>
+        <tr>
+            <td>
+                
+            </td>
+            <td>
+                Математика
+            </td>
+            <td>
+                ОБЖ
+            </td>
+            <td>
+                Физика
+            </td>
+        </tr>
+        <?php
+            get_data();
+        ?>
+    </table>
+
+</body>
+</html>
